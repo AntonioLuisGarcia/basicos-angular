0c4f3f7dd5b3e5bc9f9f89585e0cfb6a6d3828d3
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-10-20 08:34:55 +0200

    fix on imports

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,15 +3,21 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
   imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    SharedModule,
     HomePageRoutingModule,
-    SharedModule
   ],
   declarations: [
-    HomePage
+    HomePage,
   ],
   exports:[
     HomePage
