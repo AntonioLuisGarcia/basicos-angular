c8eb935d6659d5c0564a0a0c01b7df7ac531e157
daluga0503 <daniel.luquegallardo@gmail.com>
2023-10-23 19:06:17 +0200

    Formulario añadido

--- src/app/pages/user/user-info/user-info.component.ts
+++ src/app/pages/user/user-info/user-info.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from 'src/app/core/interfaces/User';
+
 
 @Component({
   selector: 'app-user-info',
@@ -8,10 +9,17 @@ import { User } from 'src/app/core/interfaces/User';
 })
 export class UserInfoComponent  implements OnInit {
 
+
   @Input() user:User | undefined;
+
+  @Output() onCardClicked:EventEmitter<void> = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {}
+
+  onCardClick(){
+    this.onCardClicked.emit();
+  }
 
 }
--- src/app/pages/user/user.page.ts
+++ src/app/pages/user/user.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
 import { User } from 'src/app/core/interfaces/User';
 import { UserServiceService } from 'src/app/core/services/user-service.service';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @Component({
   selector: 'app-user',
@@ -15,6 +17,7 @@ export class UserPage implements OnInit {
   constructor(
     private router: Router,
     private userService:UserServiceService,
+    private modal: ModalController
 
   ) { }
 
@@ -28,5 +31,70 @@ export class UserPage implements OnInit {
 
   public home(){
     this.router.navigate(['/home']);
+  }
+
+  public async onCardClicked(user:User){
+    
+    var onDismiss = (info:any)=>{
+      console.log(info);
+      switch(info.role){
+        case 'ok':{
+          /*
+          this.user.updateUser(info.data).subscribe(async user=>{
+              const options:ToastOptions = {
+              message:"User modified",
+              duration:1000,
+              position:'bottom',
+              color:'tertiary',
+              cssClass:'card-ion-toast'
+            };
+            const toast = await this.toast.create(options);
+            toast.present();
+          })
+          */
+        }
+        
+        break;
+        case 'delete':{
+          /*
+          this.users.deleteUser(info.data).subscribe(async user=>{
+            const options:ToastOptions = {
+            message:"User deleted",
+            duration:1000,
+            position:'bottom',
+            color:'tertiary',
+            cssClass:'card-ion-toast'
+          };
+          const toast = await this.toast.create(options);
+          toast.present();
+        })
+        */
+        }
+        
+        break;
+        
+        default:{
+          console.error("No debería entrar");
+        }
+      }
+    }
+    this.presentForm(user, onDismiss);
+  }
+
+  async presentForm(data:User|null, onDismiss:(result:any)=>void){
+    
+    const modal = await this.modal.create({
+      component:UserFormComponent,
+      componentProps:{
+        user:data
+      },
+      cssClass:"modal-full-right-side"
+    });
+    modal.present();
+    modal.onDidDismiss().then(result=>{
+      if(result && result.data){
+        onDismiss(result);
+      }
+    });
   }
 }
