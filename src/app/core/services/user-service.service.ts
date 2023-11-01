316200db2ed2991d7db4af26121a88ba65d1a7a7
daluga0503 <daniel.luquegallardo@gmail.com>
2023-11-01 15:40:12 +0100

    servicio usuarios modificado lectura archibo json

--- src/app/core/services/user-service.service.ts
+++ src/app/core/services/user-service.service.ts
@@ -11,12 +11,7 @@ export class UserServiceService {
 
   id:number = 4;//porque tenemos 4 usuarios hasta ahora
   private _users:BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
-  private users:User[] = [
-    {id:0, name:"Antonio Luis", surname:"García Guerrero",age:18},
-    {id:1, name:"Daniel", surname:"Luque Gallardo",age:19},
-    {id:2, name:"Jose", surname:"Perez Perez",age:30},
-    {id:3, name:"Kiko", surname:"Rivera",age:39},
-  ];
+  public users$:Observable<User[]> = this._users.asObservable();
 
   constructor(
     private http:HttpClient
@@ -26,23 +21,27 @@ export class UserServiceService {
     // Sin json server
     // this._users.next(this.users);
     // return this._users.asObservable();
-     return this.http.get<User[]>(environment.apiUrl+'/users').pipe(tap((users:any[])=>{
+      return this.http.get<User[]>(environment.apiUrl+'/users').pipe(tap((users:any[])=>{
       this._users.next(users);}));
   }
 
   updateUser(user:User):Observable<User>{
-    var users = [...this._users.value]
-    var index = users.findIndex(u => u.id == user.id);
-
-    if(index >= 0){
-      this.users[index] = user;
-      this._users.next(this.users);
-      return of(user);
-    }else{
-      return throwError("Usuario no encontrado");
-    }
+    return new Observable (observer => {
+      this.http.patch<User>(environment.apiUrl+`/users/${user.id}`,user).subscribe(_=>{
+        this.getAll().subscribe(_=>{
+          this.getUser(user.id).subscribe(_user=>{
+            observer.next(_user);
+          })
+        })
+      })
+    })
   }
 
+
+  public getUser(id:number):Observable<User>{
+    return this.http.get<User>(environment.apiUrl+`/users/${id}`);
+  }
+  /*
   getUser(user:User):Observable<User>{
     return new Observable(observer=>{
     var _user = this._users.value.findIndex(u => u.id = user.id)
@@ -53,8 +52,19 @@ export class UserServiceService {
     }
   })
   }
+  */
+  
+  deleteUser(user:User): Observable<User>{
+    return new Observable (observer =>{
+      this.http.delete<User>(environment.apiUrl + `/users/${user.id}`).subscribe(_=>{
+        this.getAll().subscribe(_=>{
+          observer.next(user);
+        })
+      })
+    })
+  }
 
-
+  /*
   deleteUser(user:User):Observable<User>{
     return new Observable(observer=>{
       var _user = [...this._users.value];
@@ -69,16 +79,33 @@ export class UserServiceService {
       observer.complete();
     })
   }
+  */
+  createUser(user:User):Observable<User>{
+    var _user: any = {
+      id: user.id,
+      name: user.name,
+      surname: user.surname,
+      age: user.age
+    }
+    return this.http.post<User>(environment.apiUrl+"/users",_user).pipe(tap(_=>{
+      this.getAll().subscribe();
+    }))
+  }
+  /*
+  createUser(user:User):Observable<User>{
+    return new Observable(observer =>{
+      var _users = [...this._users.value];
+      user.id = ++this.id;
+      _users.push(user);
+      this._users.next(_users);
+      observer.next(user);
+      })
+    }
+  }
+  */
 
 
-createUser(user:User):Observable<User>{
-  return new Observable(observer =>{
-    var _users = [...this._users.value];
-    user.id = ++this.id;
-    _users.push(user);
-    this._users.next(_users);
-    observer.next(user);
-    })
+  public query(q:string):Observable<User[]>{
+    return this.http.get<User[]>(environment.apiUrl+'/users?q='+q);
   }
 }
-
